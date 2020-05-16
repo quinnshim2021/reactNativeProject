903c82b2238aea9565a940a4a49c88278daf6194
quinnshim2021 <quinnshim2021@u.northwestern.edu>
2020-05-16 18:04:54 -0400

    react native camera expo example done

---
+++ Camera.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { Camera } from 'expo-camera'
+import { View, Text, TouchableOpacity } from 'react-native';import * as Permissions from 'expo-permissions';
+import { FontAwesome, Ionicons,MaterialCommunityIcons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
+
+class CameraPage extends React.Component {
+    state = {
+        hasPermission: null,
+        type: Camera.Constants.Type.front,
+        ref: this.camera
+      };
+
+      async componentDidMount() {
+        this.getPermissionAsync()
+      }  
+      getPermissionAsync = async () => {
+          // Camera roll Permission 
+          if (Platform.OS === 'ios') {
+            const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+            if (status !== 'granted') {
+              alert('Sorry, we need camera roll permissions to make this work!');
+            }
+          }
+          // Camera Permission
+          const { status } = await Permissions.askAsync(Permissions.CAMERA);
+          this.setState({ hasPermission: status === 'granted' });
+        }
+
+    pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        });
+        }
+
+    handleCameraType=()=>{
+        const { cameraType } = this.state
+    
+        this.setState({cameraType:
+          cameraType === Camera.Constants.Type.back
+          ? Camera.Constants.Type.front
+          : Camera.Constants.Type.back
+        })
+      }
+    
+    takePicture = async () => {
+    if (this.camera) {
+        let photo = await this.camera.takePictureAsync();
+    }
+    }
+
+    render(){
+        const { hasPermission } = this.state;
+            if (hasPermission === null) {
+            return <View />;
+            } else if (hasPermission === false) {
+            return <Text>No access to camera</Text>;
+            } else {
+            return (
+                <Camera style={{ flex: 1, width: 415 }} type={this.state.cameraType}>
+                    <View style={{flex:1, flexDirection:"row",justifyContent:"space-between",margin:20}}>
+                        <TouchableOpacity
+                            style={{
+                            alignSelf: 'flex-end',
+                            alignItems: 'center',
+                            backgroundColor: 'transparent',                  
+                            }}
+                            onPress={()=>this.pickImage()}
+                            >
+                            <Ionicons
+                                name="ios-photos"
+                                style={{ color: "#fff", fontSize: 40}}
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={{
+                            alignSelf: 'flex-end',
+                            alignItems: 'center',
+                            backgroundColor: 'transparent',
+                            }}
+                            onPress={()=>this.takePicture()}
+                            >
+                            <FontAwesome
+                                name="camera"
+                                style={{ color: "#fff", fontSize: 40}}
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={{
+                            alignSelf: 'flex-end',
+                            alignItems: 'center',
+                            backgroundColor: 'transparent',
+                            }}
+                            onPress={()=>this.handleCameraType()}
+                            >
+                            <MaterialCommunityIcons
+                                name="camera-switch"
+                                style={{ color: "#fff", fontSize: 40}}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </Camera>
+            );
+        }
+        
+    }
+}
+
+export default CameraPage; 

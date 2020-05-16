4b1addb4c578e39d72d59c817015386b83db5794
quinnshim2021 <quinnshim2021@u.northwestern.edu>
2020-05-16 18:51:07 -0400

    testing using Jest and Enzyme

--- Camera.test.js
+++ Camera.test.js
@@ -2,8 +2,17 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 import { Camera } from './Camera';
+import {shallow} from 'enzyme';
 
 test('renders correctly', () => {
   const camera = renderer.create(<Camera />).toJSON();
   expect(camera).toMatchSnapshot();
+});
+
+test("flip camera", () => {
+  const wrapper = shallow(<Camera />);
+  const firstButton = wrapper.find("TouchableOpacity").at(2);
+  
+  firstButton.simulate("click");
+  expect(wrapper.props.type).toEqual(Camera.Constants.Type.back);
 });
